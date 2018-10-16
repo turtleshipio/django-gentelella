4368d8f5dcbfc83cdb7b8e12d4cbfa969ae3f656
Seungheon Yum <syum@syum.local>
2018-10-16 20:23:24 +0900

    changed few things

--- gentelella/app/static/build/js/notify_retailer_list.js
+++ gentelella/app/static/build/js/notify_retailer_list.js
@@ -2,7 +2,6 @@ $("li.list-group-item").click(function(){
 
     span = $(this).find(".retailer_name");
     retailer_name = span.text();
-    alert(retailer_name);
     notify_id = span.attr('id');
     window.location.href= "/notify-retailer/" + notify_id + "?retailer_name=" + retailer_name;
 });
--- gentelella/app/views/notify/notify_retailer_list.py
+++ gentelella/app/views/notify/notify_retailer_list.py
@@ -12,23 +12,16 @@ class NotifyRetailerListView(ListView):
 
     def get_context_data(self, *args, **kwargs):
         notify_id = self.kwargs['notify_id']
-        print("!!!!!!!!!!!!")
-        print("!!!!!!!!!!!!")
-        print(notify_id)
-        print("!!!!!!!!!!!!")
-        print("!!!!!!!!!!!!")
-        print("!!!!!!!!!!!!")
-        print("!!!!!!!!!!!!")
         context = super(NotifyRetailerListView, self).get_context_data(*args, **kwargs)
 
         orders_dict = {}
         #orders = Order.objects.filter(notify_id=notify_id).order_by("order_id").values("retailer_name", "notify_order_id", "price", "count")
 
 
-        query = "SELECT retailer_name, notify_id, count(*) as count, SUM(price * count) as amt " \
+        query = "SELECT retailer_name, ws_name, notify_id, count(*) as count, SUM(price * count) as amt " \
                 "FROM orders " \
                 "WHERE notify_id = '{notify_id}' " \
-                "GROUP BY notify_id " \
+                "GROUP BY (retailer_name) " \
                 "ORDER BY order_id DESC ".format(notify_id=notify_id)
 
         orders = custom_db.dict_fetchall(query)
@@ -36,4 +29,5 @@ class NotifyRetailerListView(ListView):
 
         context['orders'] = orders
         context['notify_id'] = notify_id
+        context['ws_name'] = orders[0]['ws_name']
         return context
